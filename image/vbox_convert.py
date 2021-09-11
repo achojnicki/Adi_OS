69021f6bdf9f671d9c594daeafadafe344739ff9
achojnicki <adrian@chojnicki.info>
2021-09-11 15:35:38 +0100

    --

--- image/vbox_convert.py
+++ image/vbox_convert.py
@@ -12,10 +12,6 @@ attach_image(IMAGE_FILENAME)
 loop_device=get_loop_device(IMAGE_FILENAME)
 
 
-
-attach_loop_device_to_vbox_disk_file(loop_device,
-	VBOX_DISK_FILE)
-
 convert_vbox_image(loop_device,
 	VBOX_GENERATED_DISK_FILE)
 
