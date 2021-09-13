b9ed2a2ea7f86a7973a0d9dc04eeefde30e0c069
achojnicki <adrian@chojnicki.info>
2021-09-13 14:16:52 +0100

    --

--- rootfs/scripts/init/init.sh
+++ rootfs/scripts/init/init.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 
+#io rw
+mount -o remount,rw /dev/sda /
+
+#tty
+getty -L 19200 tty1 vt102&
+getty -L 19200 tty2 vt102&
+getty -L 19200 tty3 vt102&
+getty -L 19200 tty4 vt102&
+getty -L 19200 tty5 vt102&
+
+#net
 ifconfig lo 127.0.0.1
 ifconfig lo netmask 255.0.0.0
